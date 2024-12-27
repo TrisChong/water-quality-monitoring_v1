969f938b308dcafa03ad0f259546a33ff2ed251d
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-28 00:11:36 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+        target: 'https://water-quality-monitoring-api.onrender.com',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
