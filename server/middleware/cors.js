cf9e6ab17528ffe4f27fc9d7ec585943f64496fb
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 17:42:57 +0800

    Update cors.js

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -2,11 +2,12 @@ import cors from 'cors';
 
 const corsOptions = {
   origin: [
-    'https://water-quality-monitoring.onrender.com',
+    'https://animated-mooncake-33899c.netlify.app',
+    'https://water-quality-monitoring-v1.onrender.com',
     'http://localhost:5173'
   ],
-  credentials: false,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Length', 'Content-Type']
 };
