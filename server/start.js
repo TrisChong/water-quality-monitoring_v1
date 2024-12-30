7c5f59675292866229050b4befbb8d19f19b5075
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 16:51:36 +0800

    Add files via upload

--- server/start.js
+++ server/start.js
@@ -1,18 +1,21 @@
-import app from './index.js';
 import { createServer } from 'http';
-import { configureSocket } from './config/socket.js';
-import connectDB from './config/db/index.js';
 import dotenv from 'dotenv';
 import { log } from './utils/logger.js';
+import createApp from './config/app.js';
+import { configureSocket } from './config/socket.js';
+import { connectDB } from './config/db/index.js';
 
 dotenv.config();
 
 const startServer = async () => {
   try {
-    const PORT = process.env.PORT || 5000;
+    log.info('Starting server...');
     
     // Connect to MongoDB
     await connectDB(process.env.MONGODB_URI);
+    
+    // Create Express app
+    const app = createApp();
     
     // Create HTTP server
     const httpServer = createServer(app);
@@ -20,10 +23,11 @@ const startServer = async () => {
     // Configure WebSocket
     configureSocket(httpServer);
     
-    // Start listening
+    // Start server
+    const PORT = process.env.PORT || 5000;
     httpServer.listen(PORT, () => {
       log.success(`Server running on port ${PORT}`);
-      log.info(`API available at http://localhost:${PORT}/api`);
+      log.info(`API URL: http://localhost:${PORT}/api`);
     });
 
   } catch (error) {
@@ -33,5 +37,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-
