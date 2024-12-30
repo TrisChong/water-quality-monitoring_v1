8739d3f6412ff5319e797ba49073f45b46f6cced
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 16:00:14 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+import TopNavigation from './components/navigation/TopNavigation';
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <div className="min-h-screen bg-gray-100">
-          <div className="max-w-7xl mx-auto px-4">
-            <header className="bg-emerald-400 p-4 text-white text-center">
-              <h1 className="text-xl font-bold tracking-wide">
-                IOT BASED HOUSE WATER FLOW RATE, TURBIDITY, TDS LEVEL AND PH LEVEL MONITORING SYSTEM
-              </h1>
-            </header>
+        <div className="min-h-screen bg-gray-100 max-w-[425px] mx-auto">
+          <header className="bg-emerald-400 p-3 text-white text-center">
+            <h1 className="text-lg font-bold tracking-wide">
+              IOT BASED HOUSE WATER FLOW RATE, TURBIDITY, TDS LEVEL AND PH LEVEL MONITORING SYSTEM
+            </h1>
+          </header>
+          <TopNavigation />
+          <main className="p-4">
             <AppRoutes />
-          </div>
+          </main>
         </div>
       </AuthProvider>
     </Router>
