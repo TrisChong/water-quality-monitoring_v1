d8dcbc656b8bf822670ea20de15f4110eec76972
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 16:24:48 +0800

    Add files via upload

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,9 +5,7 @@ import RegisterForm from '../components/auth/RegisterForm';
 import UserDashboard from '../components/dashboard/UserDashboard';
 import AdminDashboard from '../components/dashboard/AdminDashboard';
 import UserProfile from '../components/dashboard/profile/UserProfile';
-import DataExport from '../components/dashboard/reports/DataExport';
-import UserManagement from '../components/dashboard/users/UserManagement';
-import SensorManagement from '../components/dashboard/sensors/SensorManagement';
+import Conclusion from '../components/pages/Conclusion';
 
 const AppRoutes = () => {
   return (
@@ -15,18 +13,12 @@ const AppRoutes = () => {
       {/* Public routes */}
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
+      <Route path="/conclusion" element={<Conclusion />} />
 
-      {/* User routes */}
+      {/* Protected routes */}
       <Route path="/dashboard" element={<PrivateRoute><UserDashboard /></PrivateRoute>} />
-      <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
-      <Route path="/export" element={<PrivateRoute><DataExport /></PrivateRoute>} />
-      
-      {/* Admin routes */}
       <Route path="/admin" element={<PrivateRoute requireAdmin><AdminDashboard /></PrivateRoute>} />
-      <Route path="/admin/users" element={<PrivateRoute requireAdmin><UserManagement /></PrivateRoute>} />
-      <Route path="/admin/sensors" element={<PrivateRoute requireAdmin><SensorManagement /></PrivateRoute>} />
-      <Route path="/admin/profile" element={<PrivateRoute requireAdmin><UserProfile /></PrivateRoute>} />
-      <Route path="/admin/export" element={<PrivateRoute requireAdmin><DataExport /></PrivateRoute>} />
+      <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
 
       {/* Default redirect */}
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
