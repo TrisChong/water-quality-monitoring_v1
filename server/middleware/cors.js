76511b9a19d236c56d98279c8744575b00164d65
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 18:44:56 +0800

    Update cors.js

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -7,7 +7,7 @@ const corsOptions = {
     'http://localhost:5173'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Length', 'Content-Type']
 };
