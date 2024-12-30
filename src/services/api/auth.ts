9a8ba685cfbf15413fc1113149bc374836e853be
TrisChong <73765180+TrisChong@users.noreply.github.com>
2024-12-30 17:21:52 +0800

    Add files via upload

--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -1,16 +1,22 @@
 import axios from 'axios';
-import { API_CONFIG } from '../../config/api';
+import { API_CONFIG, ENDPOINTS } from '../../config/api';
 import { LoginCredentials } from '../../types/auth';
 import { handleApiError } from '../utils/errorHandler';
 
-const api = axios.create({
-  ...API_CONFIG,
-  withCredentials: true
-});
+const api = axios.create(API_CONFIG);
 
 export const login = async (credentials: LoginCredentials) => {
   try {
-    const response = await api.post('/auth/login', credentials);
+    const response = await api.post(ENDPOINTS.AUTH.LOGIN, credentials);
+    return response.data;
+  } catch (error) {
+    throw handleApiError(error);
+  }
+};
+
+export const register = async (data: any) => {
+  try {
+    const response = await api.post(ENDPOINTS.AUTH.REGISTER, data);
     return response.data;
   } catch (error) {
     throw handleApiError(error);
